d6aa7fb05716be04cb72a0b0d5caa20315a654fb
Thayssz <juuhfreitas4333@gmail.com>
2024-02-24 18:34:21 -0300

    manipulação de arquivos

---
+++ Unidade 2/manipulando arquivos/quest1/quest1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    double nota1, nota2, nota3;
+    double media;
+    char name[100];
+    char tam[100];
+    FILE *arq1;
+    FILE *arq2;
+
+    if((arq2 = fopen("saida_q1.txt", "w")) == NULL){
+        printf("Erro ao abrir arquivo\n");
+    }
+
+    if((arq1 = fopen("entrada_q1.txt", "r")) == NULL){
+        printf("Erro ao abrir arquivo");
+    }
+    else{
+        while((fgets(tam, 100, arq1)) != NULL){
+            sscanf(tam, "%15[^\t]\t%lf\t%lf\t%lf", name, &nota1, &nota2, &nota3);
+            media = (nota1 + nota2 + nota3)/3;
+
+            fprintf(arq2, "%s\t %.1lf\t %s\n", name, media, (media >=7)?"Aprovado":"Reprovado");
+        }
+        fclose(arq1);
+        fclose(arq2);
+    }
+    return 0;
+}
